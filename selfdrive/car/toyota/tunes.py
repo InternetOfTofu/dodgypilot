38f18c7bbfc8d96698f4cb6453ac579c2d20ff42
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2022-12-02 22:45:13 +1100

    bughub desktop

--- selfdrive/car/toyota/tunes.py
+++ selfdrive/car/toyota/tunes.py
@@ -41,9 +41,9 @@ def set_long_tune(tune, name):
     tune.kpBP = [0.]
     tune.kiBP = [0.]
     tune.kdBP = [0., 5., 20., 35.]
+    tune.kpV = [1.2]
     tune.kiV = [0.36]
     tune.kdV = [1.6, 0.42, .6, .75]
-    tune.kpV = [1.2]
   # Default longitudinal tune
   elif name == LongTunes.TSSStock:
     tune.kpBP = [0., 5., 35.]
